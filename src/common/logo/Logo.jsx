941612b388c44a100b02969bfa6e16076f6238a5
mfaibrahem <mfaibrahem2013@gmail.com>
2021-11-22 11:34:52 +0200

    update dev

--- src/common/logo/Logo.jsx
+++ src/common/logo/Logo.jsx
@@ -1,16 +1,23 @@
-import './Logo.scss';
-
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
 import routerLinks from '../../components/app/routerLinks';
+import './Logo.scss';
 
 const Logo = ({ className, colored }) => {
+  const { i18n } = useTranslation();
   return (
     <Link className={className} to={routerLinks.homePage}>
       {!colored ? (
-        <img src="/assets/imgs/logo/logo.png" alt="app logo" />
-      ) : (
+        i18n.dir() === 'rtl' ? (
+          <img src="/assets/imgs/logo/logo.png" alt="app logo" />
+        ) : (
+          <img src="/assets/imgs/logo/ENG-logo.png" alt="app logo" />
+        )
+      ) : i18n.dir() === 'rtl' ? (
         <img src="/assets/imgs/logo/logo-colored.png" alt="app logo" />
+      ) : (
+        <img src="/assets/imgs/logo/ENG-logo-colored.png" alt="app logo" />
       )}
     </Link>
   );
